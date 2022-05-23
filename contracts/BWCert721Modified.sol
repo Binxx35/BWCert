d78c7ae236faeb9638250b13fa94420832f6a0a2
Inish1 <c.inish.crisson@bitwave.io>
2022-05-23 14:51:15 +0100

    Initialising truffle project, test, contracts

---
+++ contracts/BWCert721Modified.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: MIT
+
+/***
+Looking to for a crypto / web3 ERP solution?
+Search no further: www.bitwave.io
+We are hiring, check out our careers page for great opportunities!
+***/
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+ /// @title A modified ERC721 token for Bitwave Certification.
+ /// @author Inish Crisson
+ /// @author Jesse Farese
+ /// @author Kunz Mainali
+ /// @dev This contract is a modified version of the ERC721 contract.
+ /// @notice This beta contract is pending review, not for mainnet deployment.
+contract BWCert721Modified is ERC721, Ownable {
+
+    // Pending image finalisation, presently placeholder.
+    string public baseURI = "ipfs://QmXYLJ2xZ3E9oLXRxVkwSEWxog7gnRtFVttQ5aEFAL5P1N/";
+    string private baseExtension = ".json";
+
+    /// @notice A mapping to facilitate pre-approval functionality.
+    /// @notice NFT transfers require approval from Bitwave & the owner of the NFT.
+    mapping (uint => address) public bitwaveApprovals;
+
+    /// @notice Constructor, sets name and symbol.
+    constructor() ERC721("Bitwave Certificate", "BWC") {}
+
+    /// @notice iterating uint for tokenId creation.
+    uint256 internal _currentIndex;
+
+    /// @notice An internal function to iterate tokenIds.
+    function currentTokenId() private returns (uint256) {
+        return _currentIndex += 1;
+    }
+
+    /// @notice A custom function to mint NFTs in bulk. 
+    /// @notice Only callable by the owner of the contract.
+    /// @param to the array of addresses to mint the NFTs to.
+    function bulkMint(address[] memory to) public onlyOwner {
+        for (uint i = 0; i < to.length; i++) {
+            _safeMint(to[i], currentTokenId());
+        }
+    }
+
+    /// @notice Overrides the ERC721 transferFrom function to add multisig functionality.
+    /// @notice NFT transfers require approval from Bitwave & the owner of the NFT.
+    /// @param from address of the sender
+    /// @param to address of the recipient
+    /// @param tokenId the id of the token to be transferred
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        require(bitwaveApprovals[tokenId] == to, "Token transfers require Bitwave pre-approval.");
+        ERC721.transferFrom(from, to, tokenId);
+    }
+
+    /// @notice Token transfers require bitwave pre-approval.
+    /// @param tokenId the id of the token to be approved for transfer.
+    /// @param to the address to which the token is to be approved.
+    function bitwaveApprove(address to, uint256 tokenId) public onlyOwner {
+        bitwaveApprovals[tokenId] = to;
+    }
+
+    /// @notice Transfers a token to a new owner. This functions is called by the owner of the token. 
+    /// @notice This function requires pre-approval from Bitwave to run. 
+    /// @param from the address of the current owners of the token.
+    /// @param to the address of the new owner of the token.
+    /// @param tokenId the id of the token to be transfered.
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public virtual override {
+        require(bitwaveApprovals[tokenId] == to, "Token transfers require Bitwave pre-approval.");
+        bitwaveApprovals[tokenId] = address(0x0);
+        ERC721.safeTransferFrom(from, to, tokenId);
+    }
+
+    /// @notice mint a single new token
+    /// @param to the address to which the token is to be minted.
+    function mint(address to) public onlyOwner {
+        _safeMint(to, currentTokenId());
+    }
+
+    /// @notice Withdraw ether from the contract.
+    /// @notice only callable by the owner of the contract. 
+    function withdraw() external payable onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    /// @notice returns the URI of the image associated with the token.
+    /// @param tokenId the id of the token to be queried.
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId));
+        string memory stringTokenId = Strings.toString(tokenId);
+        return bytes(baseURI).length != 0 ? string(abi.encodePacked(baseURI, stringTokenId, baseExtension)) : '';
+    }
+}
